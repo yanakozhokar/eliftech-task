95e26bb9b5072490281b2bf1d9e8a12d41583162
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-01 12:50:57 +0300

    feat: add sending from data to database

--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -5,17 +5,39 @@ export const orderSlice = createSlice({
   initialState: { items: [] },
   reducers: {
     addOrderItem: (state, action) => {
-      return { ...state, items: [...state.items, action.payload] };
+      const existingItem = state.items.find(
+        item => item.name === action.payload.name
+      );
+
+      if (existingItem) {
+        existingItem.amount += 1;
+        existingItem.price += action.payload.price;
+      } else {
+        const newItem = { ...action.payload, amount: 1 };
+        state.items.push(newItem);
+      }
     },
+
     deleteOrderItem: (state, action) => {
       return {
         ...state,
         items: state.items.filter(item => item.id !== action.payload.id),
       };
     },
+
+    confirmOrder: (state, action) => {
+      fetch('https://64748ef77de100807b1b337b.mockapi.io/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(action.payload),
+      });
+    },
   },
 });
 
-export const { addOrderItem, deleteOrderItem } = orderSlice.actions;
+export const { addOrderItem, deleteOrderItem, confirmOrder } =
+  orderSlice.actions;
 
 export default orderSlice.reducer;
